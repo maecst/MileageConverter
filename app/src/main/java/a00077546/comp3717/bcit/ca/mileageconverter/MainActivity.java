255240e90c4b809a3c0d0de3313f5ee10b7dc6e6
Mae Yee <mae.cast@hotmail.com>
2015-09-25 14:29:43 -0700

    Changed UI:  changed radio buttons into two buttons.  Restructured code.

--- app/src/main/java/a00077546/comp3717/bcit/ca/mileageconverter/MainActivity.java
+++ app/src/main/java/a00077546/comp3717/bcit/ca/mileageconverter/MainActivity.java
@@ -1,17 +1,14 @@
 package a00077546.comp3717.bcit.ca.mileageconverter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.lang.String;
-
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,40 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        final Button button = (Button) findViewById(R.id.btn_convert);
-        final TextView textView = (TextView) findViewById(R.id.outputText);
-        final EditText edit = (EditText)findViewById(R.id.num2convert);
-
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
-        button.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                String input = edit.getText().toString();
-                Double inputValue = Double.parseDouble(input);
-
-                Double outputValue = convertToMiles(inputValue);
-
-                String output = outputValue.toString();
-                textView.setText(input + " KM = " + output + " Miles");
-                System.out.println("You entered: " + input);
-            }
-        });
-
         return true;
-    }
-
-    public double convertToMiles(double d) {
-        final double miles2km = 0.621371;
-        return d * miles2km;
-    }
-
-    public double convertToKM(double d) {
-        final double kmToMiles = 1.60934;
-        return d * kmToMiles;
     }
 
     @Override
@@ -73,5 +40,44 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void convertDistance(final View view) {
+
+        final TextView outputText = (TextView) findViewById(R.id.outputText);
+        final EditText edit = (EditText)findViewById(R.id.num2convert);
+
+        final String inputText = edit.getText().toString();
+        final double input = Double.parseDouble(inputText);
+        final double output;
+
+
+        if (TextUtils.isEmpty(inputText)) {
+            Toast.makeText(this, "Nothing to convert.  Try again.", Toast.LENGTH_LONG).show();
+            outputText.setText("Nothing to convert.  Try again.");
+        } else {
+            switch (view.getId()) {
+                case R.id.btn_toKM:
+                    //Toast.makeText(this, "Convert toKM", 1000).show();
+                    output = convertToKM(input);
+                    outputText.setText(inputText + " Miles = " + output + " KM");
+                    break;
+                case R.id.btn_toMiles:
+                    //Toast.makeText(this, "Convert to Miles", 1000).show();
+                    output = convertToMiles(input);
+                    outputText.setText(inputText + " KM = " + output + " Miles");
+                    break;
+            }
+        }
+    }
+
+    private double convertToMiles(final double d) {
+        final double MILES_TO_KM = 0.621371;
+        return d * MILES_TO_KM;
+    }
+
+    private double convertToKM(final double d) {
+        final double KM_TO_MILES = 1.60934;
+        return d * KM_TO_MILES;
     }
 }
